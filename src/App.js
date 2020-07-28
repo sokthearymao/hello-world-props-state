05437a18ea64f6291a0b259915984c3bcb416953
sokthearymao <68365381+sokthearymao@users.noreply.github.com>
2020-07-28 17:26:01 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Message />
-        {/* props example
-        {/* <Greet name="Bruce" heroName="Batman">
+        <Counter />
+        {/* <Message />
+        props example
+        <Greet name="Bruce" heroName="Batman">
           <p>This is children props</p>
         </Greet>
         <Greet name="Clark" heroName="Superman">
