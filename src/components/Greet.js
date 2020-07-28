772e7fc69fbab0ff9bde3eadc359da56d366fd27
sokthearymao <68365381+sokthearymao@users.noreply.github.com>
2020-07-28 16:13:08 -0600

    Update initial

---
+++ src/components/Greet.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Function syntax standard JS
+// function Greet() {
+//     return <h1> Hello Terry, punk!</h1>
+// }
+
+// Arrow functions (usually more concise)
+const Greet = () =>
+    <h1>Hello Terry, punk! </h1>
+
+export default Greet;
+
+
+//Can also do a named exports such as
+//export const Greet = () =>
+//    <h1>Hello Terry, punk! </h1>
